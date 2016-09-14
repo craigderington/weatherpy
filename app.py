989f0f6a628eb261d081aebb04fdb66287995804
Craig Derington <craig@craigderington.me>
2016-09-14 17:06:43 -0400

    Update app.py - geolookup

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, redirect, url_for, request, flash
+from flask import render_template, redirect, url_for, request, flash, session
 import config
 import requests
 import json
@@ -19,10 +19,10 @@ app.base_uri = config.WU_BASE_URI
 app.base_ac_uri = config.WU_AUTOCOMPLETE_URI
 
 # search params
-doc_format = '.json'
+format = '.json'
 
 wu_cats = {
-    "geolookup": "/geolookup/",
+    "geolookup": "/geolookup/q/",
     "satellite": "/satellite/",
     "webcams": "/webcams/",
     "conditions": "/conditions/",
@@ -41,9 +41,10 @@ def index():
 
     if request.method == 'POST':
         query = request.form['search']
+        data = get_search_data(str(query))
         return render_template(
             'index2.html',
-            query=query,
+            data=data,
         )
     else:
         return render_template(
@@ -52,32 +53,59 @@ def index():
         )
 
 
-@app.route('/geolookup', methods=['GET', 'POST'])
-def geolookup():
+@app.route('/get_location', methods=['POST'])
+def get_location():
+    session['location'] = request.form['location']
+    return redirect(url_for('location'))
+
+
+@app.route('/geolookup', methods=['GET'])
+def location():
+    location = session['location']
+    location = str.split(str(location), ' ')
+    location = str(location[0] + ',' + location[1])
+
+    geolookup = get_location_data(wu_cats.get('geolookup'), location)
+
+    """
+    conditions = get_location_data('conditions', location)
+    forecast10day = get_location_data('forecast10day', location)
+    webcams = get_location_data('webcams', location)
+    satellite = get_location_data('satellite', location)
+    """
+    context = {
+        'geolookup': geolookup
+        #'forecast10day': forecast10day,
+        #'webcams': webcams,
+        #'satellite': satellite
+    }
+
     return render_template(
-        'geolookup.html',
-        context=context
+        'location.html',
+        context=context,
     )
 
 
 def get_search_data(query):
     url = build_ac_url(app.base_ac_uri, query)
     r = requests.get(url, headers=hdr)
-    status = str(r.status_code)
     decoded = r.json()
-    return status, decoded
+    return decoded
 
 
-def get_location_data(location):
-    pass
+def get_location_data(category, query):
+    url = build_url(app.base_uri, app.api_key, category, query, format)
+    r = requests.get(url, headers=hdr)
+    location_data = r.json()
+    return location_data
 
 
 def build_url(base, key, cat, query, format):
-    return base + key + cat + urllib.urlencode(query) + format
+    return base + key + cat + query + format
 
 
 def build_ac_url(base, query):
-    return base + urllib.urlencode(query)
+    return base + urllib.quote(query)
 
 
 if __name__ == '__main__':
